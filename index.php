09a03ab87c799304b3bc3101f01d03af949d09bd
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-10-15 13:39:47 +0700

    update code

--- index.php
+++ index.php
@@ -21,9 +21,32 @@
         <div class="row" id="result">
 
         </div>
+        <div id="tpsreport"></div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js"></script>
+    <script>
+        if (annyang) {
+            // Let's define our first command. First the text we expect, and then the function it should call
+            var commands = {
+                'show tps report': function() {
+                    $('#tpsreport').animate({
+                        bottom: '-100px'
+                    });
+                }
+            };
+            annyang.init({
+                "help": function() {$("#help").show();}
+            });
+
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+
+            // Start listening. You can call this here, or attach this call to an event, button, etc.
+            annyang.start();
+        }
+    </script>
     <script>
         // su dung Jquery
         $(function() {
