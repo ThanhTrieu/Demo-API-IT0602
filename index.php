4a2299eb5779fa36007d1822797ffbd3a7ff847f
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-10-15 13:59:30 +0700

    update code

--- index.php
+++ index.php
@@ -4,88 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo API</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 offset-md-3">
-                <div class="input-group my-3">
-                    <input id="js-inputSearch" type="text" class="form-control" placeholder="Name's Movie">
-                    <button class="input-group-text btn btn-primary" id="js-btnSearch">Search</button>
-                </div>
-            </div>
-        </div>
-        <div class="row" id="result">
-
-        </div>
-        <div id="tpsreport"></div>
+        <form action="https://www.google.com/search" method="get" target="_blank" id="search-form">
+            <input name="q" type="text" placeholder="Search Google..." autocomplete="off" autofocus>
+            <!-- <button type="button"><i class="fas fa-microphone"></i></button> -->
+        </form>
+        <p class="info"></p>
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js"></script>
-    <script>
-        if (annyang) {
-            // Let's define our first command. First the text we expect, and then the function it should call
-            var commands = {
-                'show tps report': function() {
-                    $('#tpsreport').animate({
-                        bottom: '-100px'
-                    });
-                }
-            };
-            annyang.init({
-                "help": function() {$("#help").show();}
-            });
-
-            // Add our commands to annyang
-            annyang.addCommands(commands);
-
-            // Start listening. You can call this here, or attach this call to an event, button, etc.
-            annyang.start();
-        }
-    </script>
-    <script>
-        // su dung Jquery
-        $(function() {
-            let btnSearch = $('#js-btnSearch');
-            let inputSearch = $('#js-inputSearch');
-            let showResult = $('#result')
-            btnSearch.click(function() {
-                let nameMovie = inputSearch.val().trim();
-                if (nameMovie.length > 0) {
-                    $.ajax({
-                        url: "api.php",
-                        type: "POST",
-                        data: {
-                            movie: nameMovie
-                        },
-                        beforeSend: function() {
-                            $(btnSearch).text("Loading ....");
-                        },
-                        success: function(result) {
-                            $(btnSearch).text("Search");
-                            result = JSON.parse(result);
-                            showResult.html(result.map(data => `
-                                <div class="col-sm-12 col-md-3 mb-3">
-                                    <div class="card">
-                                        <img src="http://image.tmdb.org/t/p/w500${data.poster_path}" class="card-img-top" alt="${data.title}">
-                                        <div class="card-body">
-                                            <p class="card-text">${data.title}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            `).join(''));
-                        }
-                    });
-                } else {
-                    alert("Enter movie");
-                }
-            });
-        });
-    </script>
+    <script src="js/voice.js"></script>
 </body>
 
 </html>
---
+++ movie.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo API</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 offset-md-3">
+                <div class="input-group my-3">
+                    <input id="js-inputSearch" type="text" class="form-control" placeholder="Name's Movie">
+                    <button class="input-group-text btn btn-primary" id="js-btnSearch">Search</button>
+                </div>
+            </div>
+        </div>
+        <div class="row" id="result">
+
+        </div>
+        <div id="tpsreport"></div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js"></script>
+    <script>
+        if (annyang) {
+            // Let's define our first command. First the text we expect, and then the function it should call
+            var commands = {
+                'show tps report': function() {
+                    $('#tpsreport').animate({
+                        bottom: '-100px'
+                    });
+                }
+            };
+            annyang.init({
+                "help": function() {$("#help").show();}
+            });
+
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+
+            // Start listening. You can call this here, or attach this call to an event, button, etc.
+            annyang.start();
+        }
+    </script>
+    <script>
+        // su dung Jquery
+        $(function() {
+            let btnSearch = $('#js-btnSearch');
+            let inputSearch = $('#js-inputSearch');
+            let showResult = $('#result')
+            btnSearch.click(function() {
+                let nameMovie = inputSearch.val().trim();
+                if (nameMovie.length > 0) {
+                    $.ajax({
+                        url: "api.php",
+                        type: "POST",
+                        data: {
+                            movie: nameMovie
+                        },
+                        beforeSend: function() {
+                            $(btnSearch).text("Loading ....");
+                        },
+                        success: function(result) {
+                            $(btnSearch).text("Search");
+                            result = JSON.parse(result);
+                            showResult.html(result.map(data => `
+                                <div class="col-sm-12 col-md-3 mb-3">
+                                    <div class="card">
+                                        <img src="http://image.tmdb.org/t/p/w500${data.poster_path}" class="card-img-top" alt="${data.title}">
+                                        <div class="card-body">
+                                            <p class="card-text">${data.title}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            `).join(''));
+                        }
+                    });
+                } else {
+                    alert("Enter movie");
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
